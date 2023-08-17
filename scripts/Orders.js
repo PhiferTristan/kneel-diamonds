212dcdf4503b515f5c682ac96931f44ddc047c29
PhiferTristan <phifer.tristan@outlook.com>
2023-08-17 14:59:40 -0500

    now calculating and displaying order price in the order list component

--- scripts/Orders.js
+++ scripts/Orders.js
@@ -1,12 +1,15 @@
 export const Orders = async () => {
-  const fetchResponse = await fetch("http://localhost:8088/orders");
+  const fetchResponse = await fetch(
+    "http://localhost:8088/orders?_expand=metal&_expand=style&_expand=size"
+  );
   const orders = await fetchResponse.json();
 
   let ordersHTML = "";
 
   let ordersStringArray = orders.map((order) => {
+    let orderPrice = order.metal.price + order.style.price + order.size.price;
     return `<section class="order">
-            <div>Order #${order.id}</div></section>`;
+            <div>Order #${order.id} costs $${orderPrice}</div></section>`;
   });
   ordersHTML += ordersStringArray.join("");
 
