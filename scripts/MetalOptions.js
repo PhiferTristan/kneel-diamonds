c3e3e15a3da523b6d64ae55c6e40667b572b48df
PhiferTristan <phifer.tristan@outlook.com>
2023-08-17 11:19:31 -0500

    add transient state, setter function for each option, and cange listeners for each component

--- scripts/MetalOptions.js
+++ scripts/MetalOptions.js
@@ -1,6 +1,19 @@
+// The setMetalChoice() function used below is just an example.
+// Change it to the name of the setter function you created.
+import { setMetalChoice } from "./TransientState.js";
+
+const handleMetalChoice = (changeEvent) => {
+  // Make sure you change this condition if you named your inputs differently
+  if (changeEvent.target.name === "metal") {
+    setMetalChoice(parseInt(changeEvent.target.value));
+  }
+};
+
 export const metalOptions = async () => {
   const response = await fetch("http://localhost:8088/metals");
   const metals = await response.json();
+
+  document.addEventListener("change", handleMetalChoice);
 
   let metalOptionsHTML = "<h2>Metals</h2>";
 
